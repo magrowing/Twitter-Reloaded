f36c4dee2c8fe52cf6663b285989a25b2f41f02e
magrowing <kmagrowing@gmail.com>
2024-04-30 20:39:59 +0900

    feat : Routing 및 페이지 컴포넌트 생성 - home - profile - login - createAccount

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
-function App() {
-  return <></>;
-}
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-export default App;
+import Layout from './components/Layout';
+import Home from './routes/Home';
+import Profile from './routes/Profile';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '',
+        element: <Home />,
+      },
+      {
+        path: 'profile',
+        element: <Profile />,
+      },
+    ],
+  },
+]);
+
+export default function App() {
+  return <RouterProvider router={router} />;
+}
