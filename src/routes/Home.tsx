4fc8faf22c9a4ab703323616492728f37791a0cd
magrowing <kmagrowing@gmail.com>
2024-05-02 16:20:15 +0900

    feat : NavigationBar

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,15 +1,7 @@
-import { auth } from '../firebase';
-
 export default function Home() {
-  const handleLogout = () => {
-    auth.signOut();
-  };
   return (
     <>
       <h1>Home</h1>
-      <button type="button" onClick={handleLogout}>
-        Logout!
-      </button>
     </>
   );
 }
