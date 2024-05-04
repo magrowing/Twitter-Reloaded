82065a2a5026dc4be7271a882aff89d456496fc1
magrowing <kmagrowing@gmail.com>
2024-05-04 17:40:21 +0900

    feat : 실시간으로 데이터 가져올수 있도록 구현

--- src/components/TimeLine.tsx
+++ src/components/TimeLine.tsx
@@ -1,8 +1,16 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { useState, useEffect } from 'react';
 
 import styled from 'styled-components';
 
-import { collection, getDocs, orderBy, query } from 'firebase/firestore';
+import { Unsubscribe } from 'firebase/auth';
+import {
+  collection,
+  limit,
+  onSnapshot,
+  orderBy,
+  query,
+} from 'firebase/firestore';
 import { db } from '../firebase';
 
 import Tweet from './Tweet';
@@ -21,31 +29,36 @@ export interface ITweet {
 export default function TimeLine() {
   const [tweets, setTweets] = useState<ITweet[]>([]);
 
-  const fetchTweets = async () => {
-    // 컬렌션에서 tweets 데이터를 가져오는 쿼리
-    const tweetsQuery = query(
-      collection(db, 'tweets'),
-      orderBy('createdAt', 'desc') // createdAt 기준으로 최신순으로 가져오기
-    );
-    // getDocs 함수에 위에 정의한 쿼리를 적용해서 모든 문서들을 가져온다.
-    const spanshot = await getDocs(tweetsQuery);
-    const tweetsData = spanshot.docs.map((doc) => {
-      // 가져온 모든 문서들에서 데이터를 추출하고, 문서의 아이디까지 포함된 새로운 객체 생성한 후 새로운 배열로 반환
-      const { tweet, createdAt, userId, userName, photo } = doc.data();
-      return {
-        tweet,
-        createdAt,
-        userId,
-        userName,
-        photo,
-        id: doc.id,
-      };
-    });
-    setTweets(tweetsData);
-  };
-
   useEffect(() => {
+    let unsubscribe: Unsubscribe | null = null;
+    const fetchTweets = async () => {
+      // 컬렌션에서 tweets 데이터를 가져오는 쿼리
+      const tweetsQuery = query(
+        collection(db, 'tweets'),
+        orderBy('createdAt', 'desc'), // createdAt 기준으로 최신순으로 가져오기
+        limit(25) // 가져오는 쿼리의 갯수 제한
+      );
+      // 실시간으로 DB가 변경된 내용을 구독하고 있는 형태로 변경해서, 변경사항이 생기면 해당 데이터를 가져오도록 수정
+      unsubscribe = await onSnapshot(tweetsQuery, (snapshot) => {
+        const tweetsData = snapshot.docs.map((doc) => {
+          const { tweet, createdAt, userId, userName, photo } = doc.data();
+          return {
+            tweet,
+            createdAt,
+            userId,
+            userName,
+            photo,
+            id: doc.id,
+          };
+        });
+        setTweets(tweetsData);
+      });
+    };
     fetchTweets();
+    return () => {
+      // 해당 컴포넌트가 언마운트 되었을때 구독취소 함수 실행시켜주는거 추가 작업
+      unsubscribe && unsubscribe();
+    };
   }, []);
 
   return (
